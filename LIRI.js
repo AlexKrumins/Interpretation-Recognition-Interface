7292c8a976557aaaf92c10be26cace919175d029
Alex <justkrumins@gmail.com>
2019-04-27 16:42:13 -0500

    spotify functioning

--- LIRI.js
+++ LIRI.js
@@ -1,14 +1,17 @@
-var fs = require('fs');
-var axios = require('axios');
-var dotenv = require("dotenv").config();
-var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
-var omdb = new Omdb(keys.omdb);
-var bandsInTown = new BandsInTown(keys.bandsInTown);
+const fs = require('fs');
+const axios = require('axios');
+const moment = require('moment');
+require("dotenv").config();
+const keys = require("./keys.js");
+const Spotify = require('node-spotify-api');
+const spotify = new Spotify(keys.spotify);
+const omdb = keys.omdb;
+const bandsInTown = keys.bandsInTown;
 let action = process.argv[2];
 process.argv.splice(0,3);
 let input = process.argv.slice(0).join(" ");
-
+const divider = "\n------------------------------------------------------------\n\n";
+let showData = [];
 switch (action) {
     case "concert-this":
       concertFunc(input);
@@ -28,12 +31,41 @@ switch (action) {
 
     default:
       console.log("Sorry you have to type a ** command ** and then a name");
-}
-
+};
+function storeData(){
+  fs.appendFile("log.txt", showData + divider, function(err) {
+    if (err) throw err;
+    console.log(showData);
+  });
+};
 function concertFunc() {
-        var URL = "https://rest.bandsintown.com/artists/" + input + "/events?app_id=" + bandsInTown;
-    
-        axios.get(URL).then(function(response){
-            console.log(response);
-        })
+  let URL = "https://rest.bandsintown.com/artists/" + input + "/events?app_id=" + bandsInTown.app_id;
+  axios.get(URL).then(function(response){
+    jsonData = response.data[0];
+    showData = [
+      "Lineup: " + jsonData.lineup,
+      "Venue: " + jsonData.venue.name,
+      "Location: " + jsonData.venue.city + "," + jsonData.venue.region,
+      "Date of the Event: " + moment(jsonData.datetime).format("MM/DD/YYYY")
+    ].join("\n");
+    storeData();
+  })
+}
+function spotifyFunc() {
+  if (input === "") {input = "ace of base the sign"};
+  spotify.search({ type: 'track', query: input }).then(function(response) {
+    jsonData = response.tracks.items[0];
+    showData = [
+      "Song Name: " + jsonData.name,
+      "Artist: " + jsonData.artists[0].name,
+      "Album: " + jsonData.album.name,
+      "Preview Link : " + jsonData.external_urls.spotify,
+    ].join('\n');
+    storeData();
+  });
+}
+function omdbFunc() {
+  if (input === "") {input = "Mr. Nobody"};
+  let URL = ""
+  axios.get
 }
--- keys.js
+++ keys.js
@@ -1,4 +1,4 @@
-console.log('keys loaded');
+console.log('\nkeys loaded\n');
 
 exports.spotify = {
   id: process.env.SPOTIFY_ID,
